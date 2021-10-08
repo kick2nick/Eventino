0a050d01972f86832bfdc5ca5201145756cc1df8
Iogsotot <ajustusa@gmail.com>
2021-10-08 20:28:03 +0300

    add configs and folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App__header" />
+      <main>
+        <h1>The Best SPA ever!!!</h1>
+        <div>(Eventino)</div>
+      </main>
+      <footer />
     </div>
   );
 }
