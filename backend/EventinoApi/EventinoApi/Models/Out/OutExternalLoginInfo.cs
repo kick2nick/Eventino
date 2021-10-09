56ab4b76c78a61628c7f1373c87f2d1e7b6bcff4
ASP <dreammagica@gmail.com>
2021-10-09 15:33:44 +0300

    add google authentication

---
+++ backend/EventinoApi/EventinoApi/Models/Out/OutExternalLoginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventinoApi.Models.Out
+{
+    public class OutExternalLoginInfo
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string OIdProviderName { get; set; }
+        public string OId { get; set; }
+    }
+}
