c297be385a7f450649b0c9aea002bafef580ae52
Iogsotot <ajustusa@gmail.com>
2021-10-09 17:46:15 +0300

    feat: add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import Main from './components/Main/Main';
 
 const App: FC = () => {
   return (
     <div className="App">
-      <header className="App__header" />
-      <Main />
-      <footer />
+      <BrowserRouter>
+        <header className="App__header" />
+        <Main />
+        <footer />
+      </BrowserRouter>
     </div>
   );
 };
