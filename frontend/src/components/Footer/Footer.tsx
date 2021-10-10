9e12bc2b535d8c21cc6f15bbf833ea8ac0b14e43
DenezhkinIA <denezhkinia@ya.ru>
2021-10-10 23:10:14 +0300

    add footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,1 +1,14 @@
-export { };
+import React from 'react';
+import './Footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="app-footer">
+      <div className="container">
+        <div className="app-footer__text">2021, Eventino</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
