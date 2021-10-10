f819e2b76a6e81464687fe66b07cf1e14e1c3449
Vladislav Cheliadin <Vladislav_Cheliadin@epam.com>
2021-10-10 23:27:16 +0300

    some packages added

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -5,10 +5,14 @@ import FilterBar from '../FiltersBar/FilterBar';
 import { IEventCard, EventCard } from '../EventCard/EventCard';
 import eventsStore from '../../stores/EventsStore';
 import './main.scss';
+import { Input } from 'antd';
+import { Calendar } from 'react-date-range';
 
 const Main: FC = observer(() => {
   const title = 'Popular';
   const eventsPopular: Array<IEventCard> = [];
+
+  const { Search } = Input;
 
   const [allEvents, setAllEvents] = useState<any[]>([]);
 
@@ -16,10 +20,20 @@ const Main: FC = observer(() => {
     setAllEvents([...eventsStore.allEvents]);
   }, [eventsStore.allEvents]);
 
+  const onSearch = () => {
+    // to fill
+  };
+
   // fix: DRY
   return (
     <main className='main'>
       <FilterBar />
+
+      <Search placeholder="input search text" onSearch={onSearch} enterButton />
+      <Calendar
+        date={new Date()}
+        // onChange={this.handleSelect}
+      />
       <div>Здесь короче строка поиска и всякая фигня (дата)</div>
 
       <section className='events-by-type'>
