f93d103bdbda29d67e49cad7c30a8ac9a5d40e70
Nikolai_Kuksov <Nikolai_Kuksov@epam.com>
2021-10-10 14:28:21 +0300

    event Search added

---
+++ backend/EventinoApi/Domain/Entities/SearchFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class SearchFilter
+    {
+        public string TextToSearch { get; init; }
+
+        public int Skip { get; init; }
+        public int Take { get; init; }
+
+        public IReadOnlyCollection<string> Interests { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime? StartDate { get; init; }
+
+        [DataType(DataType.Date)]
+        public DateTime? EndDate { get; init; }
+    }
+}
