49be659f61de9e4d796adf8966f88e36c6ca92bb
Vlad <64403999+Vlad756@users.noreply.github.com>
2021-10-11 00:47:53 +0300

    Feature/data utiliczation (#37)
    * some packages added
    
    * fix all
    
    * added filter bar, search, calendar on mane page
    
    Co-authored-by: Vladislav Cheliadin <Vladislav_Cheliadin@epam.com>
    Co-authored-by: Iogsotot <ajustusa@gmail.com>

--- frontend/src/components/FiltersBar/FilterBar.tsx
+++ frontend/src/components/FiltersBar/FilterBar.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react';
 
 const FilterBar: FC = () => {
+
+  const interests = ['Sports', 'Outdoor', 'Games', 'Party', 'Movie', 'Music', 'Online', 'Restaurant', 'Training', 'Classes'];
+  
+
   return (
-    <div>Фильтры</div>
+    <div>
+      {interests.map((tag, i) => <button className='card__tag button is-rounded' key={i}>
+            <img className='tag__icon' src={`/icons/${tag}.svg`} />{tag}
+          </button>)}
+    </div>
   );
 };
 
--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -5,24 +5,55 @@ import FilterBar from '../FiltersBar/FilterBar';
 import { EventCard } from '../EventCard/EventCard';
 import eventsStore from '../../stores/EventsStore';
 import './main.scss';
+import { Input } from 'antd';
+import 'antd/dist/antd.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DatePicker, Space } from 'antd';
+
 
 const Main: FC = observer(() => {
   const title = 'Popular';
+  const { RangePicker } = DatePicker;
+
+  const { Search } = Input;
+
   const [allEvents, setAllEvents] = useState<any[]>([]);
+
+  const onChange = (value: any, dateString: any) => {
+    console.log('Selected Time: ', value);
+    console.log('Formatted Selected Time: ', dateString);
+  };
+  
+  const onOk = (value: any) => {
+    console.log('onOk: ', value);
+  };
 
   useEffect(() => {
     setAllEvents([...eventsStore.allEvents]);
   }, [eventsStore.allEvents]);
 
+  const onPanelChange = (date: any) => {
+    console.log(date); // native Date object
+  };
+
+  const onSearch = () => {
+    // to fill
+  };
+const size = 'default';
+  // fix: DRY
   return (
     <main className='main'>
       <FilterBar />
-      <div>Здесь короче строка поиска и всякая фигня (дата)</div>
 
-      <section className='events-by-type'>
-        <h2 className='events-by-type__title'>{title}</h2>
-        <button className='button button--more'>more events</button>
-      </section>
+      <div className="search-and-calendar">
+        <Search placeholder="SEARCH EVENT" allowClear onSearch={onSearch} className="search" />
+        <div className="site-calendar-demo-card">
+        <Space direction="vertical" size={12}>
+        <RangePicker />
+        </Space>
+        </div>
+      </div>
 
       <div className='events-by-type'>
         <h2 className='events-by-type__title'>{title}</h2>
